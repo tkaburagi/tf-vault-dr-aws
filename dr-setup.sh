ae60341528223b1f36147aff1b5f25e54f1d8fe5
Takayuki Kaburagi <t.kaburagi@me.com>
2020-05-27 11:29:07 +0900

    automation

--- dr-setup.sh
+++ dr-setup.sh
@@ -5,11 +5,14 @@ PRV_IP_1=10.10.1.50
 CA_CERT=$(cat /Users/kabu/hashicorp/certs/vaultca-hashidemos.crt.pem)
 CLIENT_CERT=$(cat /Users/kabu/hashicorp/certs/vaultvault-hashidemos.crt.pem)
 CLIENT_KEY=$(cat /Users/kabu/hashicorp/certs/vaultvault-hashidemos.key.pem)
+DR_1_URL=https://vault-dr-0.kabu.hashidemos.io
+DR_2_URL=https://vault-dr-1.kabu.hashidemos.io
+TAG=kabu_vault_eip
 
 PUB_IP_0=$(
   aws ec2 describe-addresses \
         --filters "Name=tag-key,Values=Name" \
-                  "Name=tag-value,Values=kabu_vault_eip" \
+                  "Name=tag-value,Values=${TAG}" \
         | jq '.Addresses | sort_by(.PrivateIpAddress)' \
         | jq -r '.[0].PublicIp'
   )
@@ -19,7 +22,7 @@ echo "PUB_IP_0: "${PUB_IP_0}
 PUB_IP_1=$(
   aws ec2 describe-addresses \
         --filters "Name=tag-key,Values=Name" \
-                  "Name=tag-value,Values=kabu_vault_eip" \
+                  "Name=tag-value,Values=${TAG}" \
         | jq '.Addresses | sort_by(.PrivateIpAddress)' \
         | jq -r '.[1].PublicIp'
   )
@@ -38,36 +41,36 @@ ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_1} \
   -o "StrictHostKeyChecking no" \
   VAULT_ADDR=https://${PRV_IP_1}:8200 ./vault operator init -format=json -tls-skip-verify
 
+echo "Enter the Token for Cluster 0"
+read VTOKEN_0
+
+echo "########## TOKEN 0: "${VTOKEN_0}
+
 echo "Enter the Token for Cluster 1"
 read VTOKEN_1
 
 echo "########## TOKEN 1: "${VTOKEN_1}
 
-echo "Enter the Token for Cluster 2"
-read VTOKEN_2
-
-echo "########## TOKEN 2: "${VTOKEN_2}
-
 # DR Settings
 echo "#DR Setting on Primary"
-VAULT_TOKEN=${VTOKEN_1} \
+VAULT_TOKEN=${VTOKEN_0} \
 vault write -f -tls-skip-verify \
--address=https://vault-dr-0.kabu.hashidemos.io \
+-address=${DR_0_URL} \
 sys/replication/dr/primary/enable
 
 WRAPPING_TOKEN=$(
-  VAULT_TOKEN=${VTOKEN_1}  \
+  VAULT_TOKEN=${VTOKEN_0}  \
   vault write -f  -format=json -tls-skip-verify \
-  -address=https://vault-dr-0.kabu.hashidemos.io \
+  -address=${DR_0_URL} \
   sys/replication/dr/primary/secondary-token \
   id="secondary" | jq -r '.wrap_info.token'
 )
 
 echo "#DR Setting on Secondary"
 
-VAULT_TOKEN=${VTOKEN_2} \
+VAULT_TOKEN=${VTOKEN_1} \
 vault write -f -tls-skip-verify \
--address=https://vault-dr-1.kabu.hashidemos.io \
+-address=${DR_1_URL} \
 sys/replication/dr/secondary/enable \
 token=${WRAPPING_TOKEN}
 
@@ -75,21 +78,21 @@ sleep 60
 
 # Check DR Status
 echo "#### DR0 Status"
-VAULT_TOKEN=${VTOKEN_1} vault read -tls-skip-verify \
--address=https://vault-dr-0.kabu.hashidemos.io \
+VAULT_TOKEN=${VTOKEN_0} vault read -tls-skip-verify \
+-address=${DR_0_URL} \
 sys/replication/dr/status
 
 echo "#### DR1 Status"
-VAULT_TOKEN=${VTOKEN_2} vault read -tls-skip-verify \
--address=https://vault-dr-1.kabu.hashidemos.io \
+VAULT_TOKEN=${VTOKEN_1} vault read -tls-skip-verify \
+-address=${DR_1_URL} \
 sys/replication/dr/status
 
 # Check Node Status
 echo "#### NODE0 STATUS"
-VAULT_TOKEN=${VTOKEN_1} vault status -tls-skip-verify \
--address=https://vault-dr-0.kabu.hashidemos.io
+VAULT_TOKEN=${VTOKEN_0} vault status -tls-skip-verify \
+-address=${DR_0_URL}
 
 echo "#### NODE1 STATUS"
 ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_1} \
 VAULT_TOKEN=${VTOKEN_1} vault status -tls-skip-verify \
--address=https://vault-dr-0.kabu.hashidemos.io
+-address=${DR_1_URL}
