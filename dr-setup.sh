a983f84fe1319772954cb9be3aea1b1c236f13dd
Takayuki Kaburagi <t.kaburagi@me.com>
2020-05-27 11:06:27 +0900

    automation

--- dr-setup.sh
+++ dr-setup.sh
@@ -28,15 +28,48 @@ echo "PUB_IP_1: "${PUB_IP_1}
 
 # Init Learder 1
 echo "# Init Learder 1"
-  ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_0} \
+ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_0} \
   -o "StrictHostKeyChecking no" \
   VAULT_ADDR=https://${PRV_IP_0}:8200 ./vault operator init -format=json -tls-skip-verify
 
 # Init Learder 2
 echo "# Init Learder 2"
-  ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_1} \
+ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_1} \
   -o "StrictHostKeyChecking no" \
   VAULT_ADDR=https://${PRV_IP_1}:8200 ./vault operator init -format=json -tls-skip-verify
+
+echo "Enter the Token for Cluster 1"
+read VTOKEN_1
+
+echo "########## TOKEN 1: "${VTOKEN_1}
+
+echo "Enter the Token for Cluster 2"
+read VTOKEN_2
+
+echo "########## TOKEN 2: "${VTOKEN_2}
+
+echo "#DR Setting on Primary"
+VAULT_TOKEN=${VTOKEN_1} \
+vault write -f -tls-skip-verify \
+-address=https://vault-dr-0.kabu.hashidemos.io \
+sys/replication/dr/primary/enable
+
+WRAPPING_TOKEN=$(
+  VAULT_TOKEN=${VTOKEN_1}  \
+  vault write -f  -format=json -tls-skip-verify \
+  -address=https://vault-dr-0.kabu.hashidemos.io \
+  sys/replication/dr/primary/secondary-token \
+  id="secondary" | jq -r '.wrap_info.token'
+)
+
+echo "#DR Setting on Secondary"
+
+
+VAULT_TOKEN=${VTOKEN_2} \
+vault write -f -tls-skip-verify \
+-address=https://vault-dr-1.kabu.hashidemos.io \
+sys/replication/dr/secondary/enable \
+token=${WRAPPING_TOKEN}
 
 # Check Status
 echo "#### NODE0 STATUS"
